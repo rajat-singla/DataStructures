d13a6a3a91389631f2989a6bd4538f78d1953bc5
Rajat Singla <rajatsingla.03@gmail.com>
2018-05-10 20:54:32 +0530

    Added function to reverse queue using single stack: reverseUsingStack()

--- DataStructures/QueueOperations.cpp
+++ DataStructures/QueueOperations.cpp
@@ -1,5 +1,38 @@
 #include<iostream>
+#include<queue>
+#include<stack>
 using namespace std;
 
+void reverseUsingStack(queue<int>& q){
+    stack<int> s;
+    while(!q.empty()){
+        s.push(q.front());
+        q.pop();
+    }
+
+    while(!s.empty()){
+        q.push(s.top());
+        s.pop();
+    }
+}
+
+void print(queue<int> q){
+
+    while(!q.empty()){
+        cout<<q.front()<<" ";
+        q.pop();
+    }
+    cout<<endl;
+}
 int main(){
+
+    queue<int> q;
+    for(int i=1;i<9;i++){
+        q.push(i);
+    }
+    print(q);
+
+    cout<<"Reversing queue using stack"<<endl;
+    reverseUsingStack(q);
+    print(q);
 }
