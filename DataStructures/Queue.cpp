771d9ab803f86f3e4793c50cc45705c3986fd2f8
Rajat Singla <rajatsingla.03@gmail.com>
2018-05-10 19:14:09 +0530

    Created Queue class with basic functionalities. These incluse enqueue(),dequeue(),front(),rear(),isEmpty(),size()

--- DataStructures/Queue.cpp
+++ DataStructures/Queue.cpp
@@ -1,6 +1,113 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+class Queue
+{
+    class Node
+    {
+    public:
+        int data;
+        Node* next;
 
+        Node(int data)
+        {
+            this->data=data;
+            next=NULL;
+        }
+    };
+
+    Node *F,*R;
+    int currSize;
+public:
+    Queue()
+    {
+        F=NULL;
+        R=NULL;
+        currSize=0;
+    }
+
+    bool isEmpty()
+    {
+        return F==NULL;
+    }
+
+    int size()
+    {
+        return currSize;
+    }
+
+    void enqueue(int data)
+    {
+        Node* temp = new Node(data);
+        if(isEmpty())
+        {
+            F=temp;
+            R=temp;
+        }
+        R->next=temp;
+        R=temp;
+        currSize++;
+    }
+
+    void dequeue()
+    {
+        if(isEmpty())
+        {
+            return;
+        }
+        Node* temp = F;
+        F=F->next;
+        delete temp;
+        currSize--;
+    }
+
+    int front()
+    {
+        if(isEmpty())
+        {
+            return -1;
+        }
+        return F->data;
+    }
+
+    int rear()
+    {
+        if(isEmpty())
+        {
+            return -1;
+        }
+        return R->data;
+    }
+};
+
+int main()
+{
+    Queue q;
+
+    cout<<"Enqueue"<<endl;
+    q.enqueue(1);
+    cout<<"front: "<<q.front()<<endl;
+    cout<<"rear: "<<q.rear()<<endl;
+    q.enqueue(2);
+    cout<<"front: "<<q.front()<<endl;
+    cout<<"rear: "<<q.rear()<<endl;
+    q.enqueue(3);
+    cout<<"front: "<<q.front()<<endl;
+    cout<<"rear: "<<q.rear()<<endl;
+    q.enqueue(4);
+    cout<<"front: "<<q.front()<<endl;
+    cout<<"rear: "<<q.rear()<<endl;
+    q.enqueue(5);
+    cout<<"front: "<<q.front()<<endl;
+    cout<<"rear: "<<q.rear()<<endl;
+    cout<<"size after enqueue = "<<q.size()<<endl;
+
+    cout<<"Dequeue 2 times"<<endl;
+    q.dequeue();
+    cout<<"front: "<<q.front()<<endl;
+    cout<<"rear: "<<q.rear()<<endl;
+    q.dequeue();
+    cout<<"front: "<<q.front()<<endl;
+    cout<<"rear: "<<q.rear()<<endl;
+    cout<<"size after dequeue = "<<q.size()<<endl;
 }
